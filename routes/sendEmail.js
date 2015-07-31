09d60eafe4366a2207cf2b01760567f17866f94d
Hamza Jrad <hamza.jrad@humediq.com>
2015-08-01 00:57:14 +0200

    Add sendEmail file

---
+++ routes/sendEmail.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+
+var nodemailer = require('nodemailer');
+
+/* GET email listing. */
+exports.sendEmail= function(req, res) {
+  /* Send emails*/ 
+  // create reusable transporter object using SMTP transport
+  var transporter = nodemailer.createTransport({
+    host: "ns0.ovh.net", // hostname
+    secure: false, // use SSL
+    port: 587, // port for secure SMTP
+    tls: {
+        rejectUnauthorized:false
+    },
+    auth: {
+        user: 'no-reply@youthcouncilstunisia.org',
+          pass: 'youthcouncil2015'
+    }
+  });
+
+  // setup e-mail data with unicode symbols
+  var mailOptions = {
+      from: 'Youth Council ✔ <no-reply@youthcouncilstunisia.org>', // sender address
+      to: 'hamza_jrad@yahoo.fr', // list of receivers
+      subject: 'Hello ✔', // Subject line
+      text: 'Hello world ✔', // plaintext body
+      html: '<b>Hello world ✔</b>' // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          console.log(error);
+      }else{
+          console.log('Message sent: ' + info.response);
+      }
+  });
+  //res.send('respond with a resource');
+  res.redirect('/');
+};
+
